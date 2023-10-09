e98ca429895997e1b786c7f42bf837dba3b85e0e
윤국현 <djaudwls7873@gmail.com>
2023-10-09 16:13:17 +0900

    🌈feat : pokemon type color

---
+++ src/components/Button/PokemonTypeButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const PokemonTypeButton = () => {
+  const BackgroundColor = {
+    'normal': '#949495',
+    'fighting': '#e09c41',
+    'flying': '#a3c3e7',
+    ''
+  }
+  return <div>index</div>;
+};
+
+export default PokemonTypeButton;
+
+export const S = {}
+
+
+
