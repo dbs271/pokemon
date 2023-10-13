83edd035fade3b855a58c34b93a95eac1ee40f7d
윤국현 <djaudwls7873@gmail.com>
2023-10-13 16:27:00 +0900

    🌈feat : pokeapi 받아오기

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,78 @@
-import PokemonTypeButton from "@/components/Button/PokemonTypeButton";
-import BackgroundColors from "@/util/BackgroundColor";
+"use client";
+import { axiosInstance } from "@/api/axiosInstance";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
-const page = () => {
-  const types = {
-    type: "normal",
+const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetchPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    try {
+      const res = await axiosInstance.get("pokemon?limit=100&offset=0");
+      console.log(res.data.results);
+      setPokemons(res.data.results);
+    } catch (err) {
+      console.error(err);
+    }
   };
-  const bColor = BackgroundColors[types.type];
-
   return (
-    <div>
-      <PokemonTypeButton
-        label="normal"
-        backgroundColor={bColor}
-        type={types.type}
-        width={30}
-        height={30}
-      />
-    </div>
+    <S.Article>
+      <S.Header></S.Header>
+      <S.Section>
+        <S.Pokemon>
+          {pokemons.length > 0 ? (
+            pokemons.map(({ url, name }, index) => (
+              <div key={index}>{name}</div>
+            ))
+          ) : (
+            <S.H2>포켓몬이 없습니다.</S.H2>
+          )}
+        </S.Pokemon>
+      </S.Section>
+    </S.Article>
   );
 };
 
-export default page;
+export default Home;
+
+export const S = {};
+S.Article = styled.article`
+  padding-top: 6px;
+`;
+
+S.Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+  padding: 1rem;
+  z-index: 50;
+`;
+S.Section = styled.section`
+  padding-top: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  z-index: 0;
+`;
+S.Pokemon = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  max-width: 56rem;
+`;
+S.H2 = styled.h2`
+  font-size: medium;
+  margin-bottom: 0.5rem;
+`;
